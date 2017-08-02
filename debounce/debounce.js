9f31b2ad552856c1f22a1b78af0459b8997b98d4
Mikhail Metrikin <metrikin@gmail.com>
2017-08-01 17:21:38 -0700

    Opps

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -2,9 +2,6 @@ function debounce(func, time) {
   let timeout;
 
   return function() {
-    // var context = this;
-    // var args = arguments;
-
     var funcRef = () => {
       timeout = null;
       func.apply(this, arguments);
@@ -15,22 +12,5 @@ function debounce(func, time) {
     timeout = setTimeout(funcRef, time);
   };
 }
-
-// function debounce(func, wait) {
-//   var timeout;
-//   return function() {
-//     var context = this,
-//       args = arguments;
-//
-//     var later = function() {
-//       timeout = null;
-//       func.apply(context, args);
-//     };
-//     // var callNow = immediate && !timeout;
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//     // if (callNow) func.apply(context, args);
-//   };
-// }
 
 module.exports = debounce;
